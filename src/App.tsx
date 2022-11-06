af8c2f77bca40ce51e4d1c29e897f3bbea77db95
dimonstr223 <dimstukmikolu@gmail.com>
2022-11-06 11:49:09 +0300

    route path updated

--- src/App.tsx
+++ src/App.tsx
@@ -12,11 +12,11 @@ import './scss/app.scss'
 function App() {
 	return (
 		<Routes>
-			<Route path='/' element={<MainLayout />}>
+			<Route path='react-pizza/' element={<MainLayout />}>
 				<Route path='' element={<Home />} />
-				<Route path='/cart' element={<Cart />} />
-				<Route path='/pizza/:id' element={<FullPizza />} />
-				<Route path='*' element={<NotFound />} />
+				<Route path='/react-pizza/cart' element={<Cart />} />
+				<Route path='/react-pizza/pizza/:id' element={<FullPizza />} />
+				<Route path='react-pizza/*' element={<NotFound />} />
 			</Route>
 		</Routes>
 	)
